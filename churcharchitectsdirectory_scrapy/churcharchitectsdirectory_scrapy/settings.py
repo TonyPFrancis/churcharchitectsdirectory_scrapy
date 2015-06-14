eba7bc560d3dbf7b22b79162e4dd4b00e49dd4cc
TonyPFrancis <tony.612.4u@gmail.com>
2015-06-14 15:29:32 +0530

    starting spider

---
+++ churcharchitectsdirectory_scrapy/churcharchitectsdirectory_scrapy/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for churcharchitectsdirectory_scrapy project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'churcharchitectsdirectory_scrapy'
+
+SPIDER_MODULES = ['churcharchitectsdirectory_scrapy.spiders']
+NEWSPIDER_MODULE = 'churcharchitectsdirectory_scrapy.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'churcharchitectsdirectory_scrapy (+http://www.yourdomain.com)'
